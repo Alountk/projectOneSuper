f5f414ba0fe7485625629d1980fec6385e3d4fff
Raul Marchan Catala <chan@Rauls-iMac.local>
2020-03-04 23:39:20 +0100

    modificacion de entorno mail y verificacion

--- js/form.js
+++ js/form.js
@@ -1,32 +1,33 @@
 //Selecciono las ubicaciones
-let name = document.forms[1][0];
+let name = document.forms[2][0];
 let errorsName = document.getElementById("errors-name");
-let email = document.forms[1][1];
+let email = document.forms[2][1];
 let errorsEmail = document.getElementById("errors-email");
-let password = document.forms[1][2];
+let password = document.forms[2][2];
 let errorsPassword = document.getElementById("errors-password");
-let repeatPassword = document.forms[1][3];
+let repeatPassword = document.forms[2][3];
 let errorsRepeatPassword = document.getElementById("errors-rep-pass");
-let regMail = document.forms[2][0];
+let regMail = document.forms[1][0];
 let errRegMail = document.getElementById("errors-mail-registrado");
-let regPass = document.forms[2][1];
+let regPass = document.forms[1][1];
 let errRegPass = document.getElementById("errors-password-registrado");
-let signupButton = document.getElementById("signup-button");
+let signupButton = document.getElementById("register-button");
 let loginButton = document.getElementById("login-button");
 //Asigno el localStorage
 let usersDB = JSON.parse(localStorage.getItem("users"));
 let currentUsersDB = JSON.parse(localStorage.getItem("currentUsers"));
+
 //Listener + blur
 
-name.addEventListener("blur", nameVerify, true);
-email.addEventListener("blur", emailVerify, true);
-password.addEventListener("blur", passwordVerify, true);
+// name.addEventListener("blur", nameVerify, true);
+// email.addEventListener("blur", emailVerify, true);
+// password.addEventListener("blur", passwordVerify, true);
 
 //Boton de registro
 signupButton.addEventListener("click", function(event) {
   event.preventDefault();
   if (validandoRegistro()){
-  if(checkEmailInDB(email)){
+    if(checkEmailInDB(email)){
       window.alert("Email ya utilizado");
       return;
     }else{
@@ -39,8 +40,19 @@ signupButton.addEventListener("click", function(event) {
 //Boton de Login
 loginButton.addEventListener("click", event => {
   event.preventDefault();
-  checkEmailAndPassword(regMail.value,regPass.value);
-})
+  if (checkEmailAndPassword(regMail.value,regPass.value)){
+    if(!checkInDBLogin(regMail.value,regPass.value)){
+      console.log("esta bien el correo");
+      // ;
+      
+
+    }else{
+      console.log("llego al else esta mal el correo");
+      return;
+    }
+  }
+  
+});
 
 
 
@@ -134,45 +146,45 @@ function checkEmailInDB (email){
 }
 
 // Eventos de relleno
-function nameVerify() {
-  if (name.value != "") {
-    name.style.border = "1px solid #5e6e66";
-    document.getElementById("name").style.color = "#5e6e66";
-    errorsName.innerHTML = "";
-    return true;
-  }
-}
-function userVerify() {
-  if (email.value != "") {
-    email.style.border = "1px solid #5e6e66";
-    document.getElementById("usuario").style.color = "#5e6e66";
-    errorsUsuario.innerHTML = "";
-    return true;
-  }
-}
-function emailVerify() {
-  if (email.value != "") {
-    email.style.border = "1px solid #5e6e66";
-    document.getElementById("email").style.color = "#5e6e66";
-    errorsEmail.innerHTML = "";
-    return true;
-  }
-}
-function passwordVerify() {
-  if (password.value != "") {
-    password.style.border = "1px solid #5e6e66";
-    document.getElementById("repeat-password").style.color = "#5e6e66";
-    document.getElementById("password").style.color = "#5e6e66";
-    errorsPassword.innerHTML = "";
-    return true;
-  }
-  if (password.value === repeatPassword.value) {
-    password.style.border = "1px solid #5e6e66";
-    document.getElementById("repeat-password").style.color = "#5e6e66";
-    errorsPassword.innerHTML = "";
-    return true;
-  }
-}
+// function nameVerify() {
+//   if (name.value != "") {
+//     name.style.border = "1px solid #5e6e66";
+//     document.getElementById("name").style.color = "#5e6e66";
+//     errorsName.innerHTML = "";
+//     return true;
+//   }
+// }
+// function userVerify() {
+//   if (email.value != "") {
+//     email.style.border = "1px solid #5e6e66";
+//     document.getElementById("usuario").style.color = "#5e6e66";
+//     errorsUsuario.innerHTML = "";
+//     return true;
+//   }
+// }
+// function emailVerify() {
+//   if (email.value != "") {
+//     email.style.border = "1px solid #5e6e66";
+//     document.getElementById("email").style.color = "#5e6e66";
+//     errorsEmail.innerHTML = "";
+//     return true;
+//   }
+// }
+// function passwordVerify() {
+//   if (password.value != "") {
+//     password.style.border = "1px solid #5e6e66";
+//     document.getElementById("repeat-password").style.color = "#5e6e66";
+//     document.getElementById("password").style.color = "#5e6e66";
+//     errorsPassword.innerHTML = "";
+//     return true;
+//   }
+//   if (password.value === repeatPassword.value) {
+//     password.style.border = "1px solid #5e6e66";
+//     document.getElementById("repeat-password").style.color = "#5e6e66";
+//     errorsPassword.innerHTML = "";
+//     return true;
+//   }
+// }
 //Crear current user
 function crearCurrentUser(email, password) {
   console.log("llego aquí");
@@ -187,46 +199,51 @@ function crearCurrentUser(email, password) {
 }
 
 //Chequear 
-function checkEmailAndPassword(email,pass){
+function checkEmailAndPassword(){
   if (email === ""){
-    errRegMail.style.border = "1px solid red";
+    console.log("1");
     errRegMail.textContent = "Email esta vacio";
     errRegMail.style.color = "red";
     regMail.focus();
-    console.log("Email esta vacio");
-    return;
-  }else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(email)) {
-    errRegMail.style.border = "1px solid red";
+    return false;
+  }
+  if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(regMail.value)) {
+    console.log("2");
     errRegMail.textContent = "El mail no se ha escrito de manera correcta";
     errRegMail.style.color = "red";
     regMail.focus();
     regMail.value="";
-    return;
-  }else if(pass === ""){
-    errRegPass.style.border = "1px solid red";
+    return false;
+  }
+  if(regPass.value === ""){
+    console.log("3");
     errRegPass.textContent = "Password esta vacio";
     errRegPass.style.color = "red";
     regPass.focus();
-    return;
+    return false;
   }
+  return true;
+}
+function checkInDBLogin(email,pass){
   mailAndPass = usersDB.map(user => {
-    if (!email===user.email){
-      errRegMail.style.border = "1px solid red";
+    if (email!=user.email){
+      console.log("4");
       errRegMail.textContent = "Este mail no existe";
       errRegMail.style.color = "red";
       regMail.value="";
       regMail.focus();
-      return;
+      return false;
       
-    }else if(!pass==user.password){
-      errRegPass.style.border = "1px solid red";
-      errRegPass.textContent = "Password esta vacio";
+    }
+    if(pass!=user.password){
+      console.log("5");
+      errRegPass.textContent = "Password incorrecto";
       errRegPass.style.color = "red";
       regPass.focus();
-      return;
-    }else{
-      crearCurrentUser(email, pass);
+      return false;
     }
-  });
-
+  crearCurrentUser(regMail.value,regPass.value)
+  console.log("llego al return true");
+	return true;
+});
 }
