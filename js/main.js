a867585c81ac44d8d8a5b935b9b865f93ae99bb8
Raul Marchan <alountk@gmail.com>
2020-03-05 13:33:42 +0100

    busqueda 1

--- js/main.js
+++ js/main.js
@@ -24,7 +24,6 @@ function callSuperHeroe1(){
           url=data.image.url;
           cambiarPortada(url,counter);
           cambiarNombre(name,counter);
-          console.log(data)
           counter++;
 
       })
@@ -42,25 +41,8 @@ function cambiarNombre(name,numeroPJ){
 function randomSuperPortada(){
     callSuperHeroe1();
 }
-function comicVine(){
-  const tokenCV="aead5e3fc45f4a8aaade28c0cdc6a27210eb7b39";
-  fetch(`https://comicvine.gamespot.com/api/characters/?format=json&sort=batman&api_key=aead5e3fc45f4a8aaade28c0cdc6a27210eb7b39`,{ mode: 'no-cors'})
-      .then((response) => {
-         const contentType = response.headers.get('content-type');
-         if (!contentType || !contentType.includes('application/json')) {
-           throw new TypeError("Oops, we haven't got JSON!");
-         }
-        response.json();
-      })
-      .then((data) => {
-          /* process your data further */
-          console.log(data);
-
-      })
-}
 
 
 
 
 randomSuperPortada();
-comicVine();
--- js/search.js
+++ js/search.js
@@ -1,64 +1,64 @@
-
-let datosSearch = document.forms[0][0];
-let buttonSearch = document.getElementById("butSearch");
-buttonSearch.addEventListener("click",function(event) {
+let datosSearch = document.forms[1][0];
+let buttonSearch = document.getElementById("btn-search");
+let busCont = document.getElementById("busqueda");
+buttonSearch.addEventListener("click", function(event) {
+  console.log("llego al boton");
   event.preventDefault();
   search(datosSearch.value);
 });
 
+function search(nameSearch) {
+  fetch(
+    `https://www.superheroapi.com/api.php/10219098893255282/search/${nameSearch}`
+  )
+    .then(response => {
+      const contentType = response.headers.get("content-type");
+      if (!contentType || !contentType.includes("application/json")) {
+        throw new TypeError("Oops, we haven't got JSON!");
+      }
+      return response.json();
+    })
+    .then(data => {
+      /* process your data further */
+      data.results.forEach(info => {
+        console.log(info);
+        var img = info.image.url;
+        let divTest=document.createElement('div');
+        divTest.innerHTML = `<div><img class="HV-img" src="${img}" alt=""><p>Nombre completo: ${info.biography["full-name"]} <br>Publisher:${info.biography.publisher}<br>Primera aparicion: ${info.biography["first-appearance"]}<br>Ocupación:${info.work.occupation}<br>Base de operaciones:${info.work.base} </p></div>`;
+        busCont.appendChild(divTest);
+      });
 
+      // let counter=1;
+      // for (i=0;i<data.results.length;i++){
+      //   console.log("llego "+i);
+      //   url=data.results.image.url;
+      //   // doesFileExist(url);
+      //   name=data.name;
+      //   console.log(name);
+      //   const fN="full-name";
+      //   fullName=data.biography[fN];
+      //   altura=data.appearance.height;
+      //   const fA="first-appearance";
+      //   primeraAparicion=data.biography[fA];
+      //   let divInfo =`<div><img src="${url}"><p>Nombre completo: ${fullName} <br>Publisher:${data.biography.publisher}<br>Primera aparicion: ${primeraAparicion}<br>Ocupación:${data.work.occupation}<br>Base de operaciones:${data.work.base} </p></div>`;
+      //   console.log(busCont);
+      //   busCont.appendChild(divInfo);
 
-
-function search(nameSearch){
-        fetch(`https://www.superheroapi.com/api.php/10219098893255282/search/${nameSearch}`)
-      .then((response) => {
-        console.log(response);
-         const contentType = response.headers.get('content-type');
-         if (!contentType || !contentType.includes('application/json')) {
-           throw new TypeError("Oops, we haven't got JSON!");
-         }
-         return response.json();
-      })
-      .then((data) => {
-          /* process your data further */
-          console.log(data.results);
-          console.log(data.results.length);
-        ventanaNueva("./search.html",nameSearch);
-        let counter=1;
-        for (i=0;i<data.results.length;i++){
-          url=data.results.image.url;
-          // doesFileExist(url);
-          name=data.name;
-          cambiarNombre2(name,counter);
-          cambiarImagenes(url,counter);
-          const fN="full-name";
-          fullName=data.biography[fN];
-          altura=data.appearance.height;
-          const fA="first-appearance";
-          primeraAparicion=data.biography[fA];
-          let divInfo =`<div><p>Nombre completo: ${fullName} <br>Publisher:${data.biography.publisher}<br>Primera aparicion: ${primeraAparicion}<br>Ocupación:${data.work.occupation}<br>Base de operaciones:${data.work.base} </p></div>`;
-          intDiv(divInfo,counter);
-        }
-
-          
-          
-
-      })
-      .catch((error) => console.error(error));    
-    
+      // }
+    })
+    .catch(error => console.error(error));
+  document.querySelector(".busqueda").classList.remove("d-none");
 }
-function ventanaNueva (url,name){
-  window.open(url,`Resultado de busqueda ${name}`,)
-  }
-function intDiv(div,numeroPJ){
-  let info = document.querySelector(".div"+numeroPJ);
-  info.innerHTML=div;
+
+function intDiv(div, numeroPJ) {
+  let info = document.querySelector(".div" + numeroPJ);
+  info.innerHTML = div;
 }
-function cambiarNombre2(nombre,numeroPJ){
-  let info = document.querySelector(".h3"+numeroPJ);
-  info.innerHTML=nombre;
+function cambiarNombre2(nombre, numeroPJ) {
+  let info = document.querySelector(".h3" + numeroPJ);
+  info.innerHTML = nombre;
 }
-function cambiarImagenes(url,numeroPJ){
-  let img = document.querySelector(".img"+numeroPJ);
-  img.src=url;
+function cambiarImagenes(url, numeroPJ) {
+  let img = document.querySelector(".img" + numeroPJ);
+  img.src = url;
 }
